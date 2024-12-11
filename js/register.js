0a6cbfbcf6f608886d34817ea09f684ef9092332
Erick Jonathan <65685466+Erickjonatthan@users.noreply.github.com>
2024-12-11 14:39:09 -0300

    Atualiza URLs de API para o servidor de produção

--- js/register.js
+++ js/register.js
@@ -12,7 +12,7 @@ document.getElementById('registrationForm').addEventListener('submit', async fun
     };
 
     try {
-        const response = await fetch('http://localhost:8080/cadastro', {
+        const response = await fetch('http://https://alfa-educa-server.onrender.com/cadastro', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
